0b73fd2efb72dcee48e6c9b5a3543b517935f2b4
ingchris10 <ingchrist10@gmail.com>
2025-07-09 23:17:13 +0000

    chore: Remove frontend node_modules and .next from git

--- codebase/frontend/.next/static/chunks/src_app_layout_tsx_007ca514._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__3c6b30e2._.css",
-  "static/chunks/node_modules_65e94ef8._.js",
-  "static/chunks/src_3fb3c19c._.js"
-],
-    source: "dynamic"
-});
